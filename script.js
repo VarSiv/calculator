8c2ba35fabfe802725cb2203621cc83e3d57985f
Varvara <var.mironov@gmail.com>
2023-07-06 22:52:14 -0300

    IT FUCKING WORKS
    THE JAVASCRIPT ACTUALLY LINKED CORRECTLY AND CAN DO STUFF WOO

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+
+
+class Calculator{
+  constructor (previousOperandTextElement, currentOperandTextElement){
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+  }
+
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+  }
+  appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+
+  }
+  updateDisplay(){
+    console.log(this.currentOperand)
+    this.currentOperandTextElement.innerHTML= this.currentOperand
+  }
+}
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -5,32 +30,12 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
-
-class Calculator {
-    constructor (previousOperandTextElement, currentOperandTextElement) {
-        this.current = currentOperandTextElement
-        this.previous = previousOperandTextElement
-        this.clear()
-    }
-    clear(){
-        this.current = ''
-        this.previous = ''
-        this.operation = undefined
-    }
-    delete(){
-
-    }
-    appendNumber(number){
-
-    }
-    chooseOperation(operation){
-
-    }
-    compute(){
-
-    }
-    updateDisplay(){
-
-    }
-}
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+  
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+  
